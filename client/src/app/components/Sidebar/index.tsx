f35235af3bc4602e85a39f2e9c5380c65ff9d6c0
Abhishek Bhattarai <abhishekbhattarai2017@gmail.com>
2025-07-11 10:57:39 +0545

    Add Sidebar component

---
+++ client/src/app/components/Sidebar/index.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsSidebarCollapsed } from "@/state";
+import {
+  Archive,
+  CircleDollarSign,
+  Clipboard,
+  Layout,
+  LucideIcon,
+  Menu,
+  SlidersHorizontal,
+  User,
+}
