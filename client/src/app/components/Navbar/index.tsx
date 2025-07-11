81835f1df0b3fdb8a7230eff0aa4456a96cd4d4a
Abhishek Bhattarai <abhishekbhattarai2017@gmail.com>
2025-07-11 10:30:35 +0545

    Navbar layout in progress – needs links and styling

--- client/src/app/components/Navbar/index.tsx
+++ client/src/app/components/Navbar/index.tsx
@@ -45,3 +45,34 @@ const Navbar = () => {
           </div>
         </div>
       </div>
+      
+      {/* RIGHT SIDE */}
+      <div className="flex justify-between items-center gap-5">
+        <div className="hidden md:flex justify-between items-center gap-5">
+          <div>
+            <button onClick={toggleDarkMode}>
+              {isDarkMode ? (
+                <Sun className="cursor-pointer text-gray-500" size={24} />
+              ) : (
+                <Moon className="cursor-pointer text-gray-500" size={24} />
+              )}
+            </button>
+          </div>
+          <div className="relative">
+            <Bell className="cursor-pointer text-gray-500" size={24} />
+            <span className="absolute -top-2 -right-2 inline-flex items-center justify-center px-[0.4rem] py-1 text-xs font-semibold leading-none text-red-100 bg-red-400 rounded-full">
+              3
+            </span>
+          </div>
+          <hr className="w-0 h-7 border border-solid border-l border-gray-300 mx-3" />
+          <div className="flex items-center gap-3 cursor-pointer">
+            <Image
+              src="https://s3-inventorymanagement.s3.us-east-2.amazonaws.com/profile.jpg"
+              alt="Profile"
+              width={50}
+              height={50}
+              className="rounded-full h-full object-cover"
+            />
+            <span className="font-semibold">Ed Roh</span>
+          </div>
+        </div>
